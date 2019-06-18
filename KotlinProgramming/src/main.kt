37f6cc46aea643e551d2c0b84785de43f26b35fe
keshar <paudelkeshar2055@gmail.com>
2019-06-18 14:31:19 +0545

    Data types in Kotlin

---
+++ KotlinProgramming/src/main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var name: String
+    name = "Kevin"
+    var age: Int = 18
+    var isAlive: Boolean = true
+    var marks: Float = 97.4F
+    var percentage: Double = 90.78
+    var gender: Char = 'M'
+
+    println(name)
+    println(age)
+    println(isAlive)
+    println(marks)
+    println(percentage)
+    println(gender)
+
+}
