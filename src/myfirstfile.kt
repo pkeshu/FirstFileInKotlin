6ccb23ecdd1a7bae24865d8278e280e6a080d89e
keshar <paudelkeshar2055@gmail.com>
2019-06-18 09:33:01 +0545

    Comments in program file.

--- src/myfirstfile.kt
+++ src/myfirstfile.kt
@@ -1,3 +1,9 @@
+/*
+* This is the main function. This is the entry point of application
+* */
 fun main(args: Array<String>) {
-    print("Hello keshar")
+    println("Hello keshar")
+    println("Hello Kotlin")
+    println(10.0 / 9)
+    print(true)
 }
